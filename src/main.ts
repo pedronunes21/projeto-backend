013b834f7f2c60f9dde2465f4f582565db85b62a
pedronunes21 <pedro.neto72pn@gmail.com>
2023-11-28 21:15:14 -0300

    exposing port

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }))
 
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, "0.0.0.0");
 }
 bootstrap();
